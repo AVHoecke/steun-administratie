2bc7fec1b5ec07f0cfb1b95afa1d0e0fe1e243d6
AVHoecke <arnovanhoecke@gmail.com>
2021-07-07 10:05:05 +0200

    Gezinnen overzicht, met msSql connectie.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,11 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="VWG steun app" />
-    <meta name="author" content="Sheikh Heera" />
+    <meta name="author" content="AVHoecke" />
 
-    <title>LaraPress</title>
+    <title>VWG Steun App</title>
 
-    <!-- Bootstrap core CSS -->
+    <!-- Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
